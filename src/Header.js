8482a4c1d537bdf517e859e49144e31b7e0339d5
Zahra Karimi <thezahrakarimi1@gmail.com>
2024-10-29 14:10:30 -0600

    changes

--- src/Header.js
+++ src/Header.js
@@ -3,19 +3,34 @@ import React from 'react';
 const Header = () => {
     return (
         <div>
-        <header className="flex justify-between items-center p-4 bg-gray-800 shadow-md">
-            <div className="text-white text-xl font-bold">
-                Your Logo
+        <header className="flex justify-between items-center p-4 bg-white opacity-50 shadow-md">
+            <div className="text-black text-xl font-bold">
+               STOPIFY
             </div>
             <nav>
                 <ul className="flex space-x-6">
                     <li>
-                        <a href="#login" className="text-white hover:underline">
+                        <a href="#signup" className="text-black hover:underline">
+                            Home
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#signup" className="text-black hover:underline">
+                            Contact Us
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#login" className="text-black hover:underline">
+                            About Us
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#signup" className="text-black hover:underline">
                             Login
                         </a>
                     </li>
                     <li>
-                        <a href="#signup" className="text-white hover:underline">
+                        <a href="#signup" className="text-black hover:underline">
                             Sign Up
                         </a>
                     </li>
@@ -24,8 +39,8 @@ const Header = () => {
         </header>
 
         {/* hero section*/}
-        <main className="flex-grow flex flex-col items-center justify-center bg-gray-100 py-20"> {/* Changed to h-screen */}
-        <div className="text-center">
+        <main className="flex-grow flex flex-col items-center justify-center bg-cover bg-center h-3/4" style={{ backgroundImage: "url('../public/images/DepartureStation.jpg')" }}> {/* Replace with your image URL */}
+        <div className="text-center bg-white bg-opacity-70 p-6 rounded-lg">
           <h2 className="text-3xl font-semibold mb-4">Welcome to Our Site</h2>
           <p className="mb-6">Find what you're looking for:</p>
           <div className="flex justify-center">
