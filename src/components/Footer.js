ee6324e484c6a5b0c3027aa21b09713934d76674
Zahra Karimi <thezahrakarimi1@gmail.com>
2024-10-30 15:21:03 -0600

    added the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
-const Footer = () => {
+  const Footer = () => {
     return (
-        <footer className="bg-gray-800 text-white text-center p-4">
-            <p>&copy; 2024 Calgary Transit App. </p>
-        </footer>
+      <footer className="bg-gray-600 text-white h-40 py-10 mt-10 flex items-center justify-center">
+        <p className="text-center">
+          &copy; {new Date().getFullYear()} STOPify. All rights reserved.
+        </p>
+      </footer>
     );
-};
-
-export default Footer;
+  };
+  
+  export default Footer;
+  
