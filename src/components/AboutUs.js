175b62f67ab9959ea3f8e162c15ff7afc31c3aa7
Zahra Karimi <thezahrakarimi1@gmail.com>
2024-10-31 17:44:08 -0600

    changes

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -5,13 +5,14 @@ import React from 'react';
         <div className="sm:flex items-center max-w-screen-xl">
             <div className="sm:w-1/2 p-10">
                 <div className="image object-center text-center">
-                    <img src="https://i.imgur.com/WbQnbas.png"/>
+                    <img src="/images/Team.png"/>
+                    {/* <img src="https://i.imgur.com/WbQnbas.png"/> */}
                 </div>
             </div>
             <div className="sm:w-1/2 p-5">
                 <div className="text">
-                    <span className="text-gray-500 border-b-2 border-blue-600 uppercase">About us</span>
-                    <h2 className="my-4 font-bold text-3xl  sm:text-4xl ">About <span className="text-blue-500">Our Company</span>
+                    <span className="text-gray-500 border-b-2 border-red-600 uppercase">About us</span>
+                    <h2 className="my-4 font-bold text-3xl  sm:text-4xl ">About <span className="text-red-500">Our Company</span>
                     </h2>
                     <p className="text-gray-700">
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, commodi
--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -10,7 +10,7 @@ import React from 'react';
         </div>
         <div className="sm:w-1/2 p-5">
             <div className="text">
-                <span className="text-gray-500 border-b-2 border-blue-600 uppercase">Contactt us</span>
+                <span className="text-gray-500 border-b-2 border-red-600 uppercase">Contactt us</span>
                 <form className="space-y-8 my-10" action="" method="post">
                <div>
                      <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
@@ -25,7 +25,7 @@ import React from 'react';
                      <textarea id="message" rows="6" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Leave a comment..."></textarea>
                  </div>
                     
-                 <button type="submit" className="w-40 bg-blue-500 text-white px-6 py-3 font-xl rounded-md sm:mb-0">Send Message</button>
+                 <button type="submit" className="w-40 bg-red-500 text-white px-6 py-3 font-xl rounded-md sm:mb-0">Send Message</button>
                     
              </form>      
             </div>
