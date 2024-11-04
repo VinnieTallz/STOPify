df85f8320454fcf31f48d0dec9de189a8f6bd1cc
VinnieTallz <vin.saini@gmail.com>
2024-11-04 01:05:29 -0700

    ignored import and call of reportWebVitals. Causing errors.  Working good now

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+//import reportWebVitals from './reportWebVitals';
 <link href="https://fonts.googleapis.com/css2?family=Racing+Sans+One&display=swap" rel="stylesheet"></link>
 
 
@@ -19,4 +19,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals();
