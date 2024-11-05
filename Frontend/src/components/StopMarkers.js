3a1cb0b107e9c06227633efd8918668c5c331697
waterl0u <jumalon.melanielou@gmail.com>
2024-11-05 14:07:45 -0700

    we fixed sstuff

--- Frontend/src/components/StopMarkers.js
+++ Frontend/src/components/StopMarkers.js
@@ -2,9 +2,7 @@ import React from 'react';
 import coordinatesArray from './testDataStops';
 import { AdvancedMarker } from '@vis.gl/react-google-maps';
 
-<<<<<<< HEAD
-const StopMarkers = () => { 
-=======
+
 const StopMarkers = () => {
   return testDataStops.map((marker, index) => (
     <AdvancedMarker
@@ -20,18 +18,6 @@ const StopMarkers = () => {
     </AdvancedMarker>
   ));
 };
->>>>>>> 601dfcc5602815d4a5fad7d312bf7ab060da0a45
 
-   const stopList= fetch ("http://localhost:3000/api/transitStops?stop_number=9686")
-
-    return coordinatesArray.map((marker, index) => (
-      <AdvancedMarker 
-        key={index} // Important: Add a unique key for each marker
-        position={{ lat: marker[1], lng: marker[0] }}
-        gmpClickable="true">
-        <img src="/images/bustop_blue.webp" alt="User Location Icon" width={30} height={35} />
-      </AdvancedMarker>
-    ));
-  };
   
   export default StopMarkers;
