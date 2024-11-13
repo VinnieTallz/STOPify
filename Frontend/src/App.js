6a00e2180cbd11d0e2d2fce380f45dfddacc8bf0
Naiara <naiaralopess@gmail.com>
2024-11-13 09:59:52 -0700

    center the map component

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -2,17 +2,17 @@ import React from 'react';
 import Header from './components/Header.js';
 import Main from './components/Main.js';
 import Footer from './components/Footer.js';
-import './index.css'; 
+import './index.css';
 
 const App = () => {
     return (
         <div className="flex flex-col min-h-screen bg-gray-100">
-        <Header/>
-            <main className="flex-grow p-4">
+            <Header />
+            <main className="flex justify-center items-center flex-grow p-4">
                 <Main />
             </main>
             <Footer />
-        </div> 
+        </div>
     );
 };
 export default App;
