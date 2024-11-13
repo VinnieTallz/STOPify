76f4ad3be8fc3f68071b7dfe524001baa91a20f8
Naiara <naiaralopess@gmail.com>
2024-11-13 10:25:51 -0700

    map size for mobile view

--- Frontend/src/components/Map.js
+++ Frontend/src/components/Map.js
@@ -36,14 +36,14 @@ const MainMap = () => {
         className="shadow-md rounded-lg p-2 mb-5 w-full focus:outline-none focus:ring-2 focus:ring-sky-500"
       />
 
-      <div className='flex flex-col-reverse h-[600px] max-h-screen sm:flex-row'>
+      <div className='flex flex-col-reverse h-full w-full sm:flex-row sm:h-[600px]'>
         <div className="flex flex-col w-full shadow-lg sm:w-1/2  ">
           <BustStopList />
         </div>
         <div className="flex flex-col md:flex-row h-full w-full md:w-2/3 shadow-lg ">
           <APIProvider apiKey={'AIzaSyA4u5WHz6-4ldEWPwyrjjjhhtkOwVm1lyo'} onLoad={() => console.log("API Loaded")}>
             {userLocation && ( // Render the map when userLocation is available
-              <div className="rounded-t-lg sm:rounded-r-lg overflow-hidden w-full h-full ">
+              <div className="rounded-t-lg sm:rounded-r-lg overflow-hidden w-full h-[400px] sm:h-full">
                 <Map
                   className="w-full h-full"
                   defaultZoom={15}
