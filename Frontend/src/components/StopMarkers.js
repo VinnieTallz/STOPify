9269e31ee950d8c8d36fc67f410eef65eaf4f240
VinnieTallz <vin.saini@gmail.com>
2024-11-13 08:26:59 -0700

    map shows markers of 'stops ner me'

--- Frontend/src/components/StopMarkers.js
+++ Frontend/src/components/StopMarkers.js
@@ -1,14 +1,68 @@
-import React from 'react';
-import { AdvancedMarker } from '@vis.gl/react-google-maps';
+import React, { useEffect, useState } from "react";
+import { AdvancedMarker } from "@vis.gl/react-google-maps";
 
-const StopMarkers = ({ testDataStops }) => {
-  // Make sure testDataStops is an array and handle cases where it might not be
-  const markers = Array.isArray(testDataStops) ? testDataStops : [];
+const StopMarkers = () => {
+  const [userLocation, setUserLocation] = useState(null);
+  const [busStops, setBusStops] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [selectedBusStop, setSelectedBusStop] = useState(null);
 
-  return markers.map((marker, index) => (
+  // Function to fetch bus stops based on user's location
+  useEffect(() => {
+    async function fetchBusStops(lat, lng) {
+      const radius = 5000;
+      try {
+        const response = await fetch(
+          `http://localhost:3000/api/busStops/nearby?lat=${lat}&lng=${lng}&radius=${radius}`
+        );
+
+        if (response.status === 200) {
+          const data = await response.json();
+          const limitedData = data.slice(0, 10);
+          setBusStops(limitedData);
+          setLoading(false);
+        } else {
+          setError("Failed to fetch bus stops.");
+          setLoading(false);
+        }
+      } catch (error) {
+        console.error("Error fetching bus stops:", error);
+        setError("Unable to retrieve bus stops.");
+        setLoading(false);
+      }
+    }
+
+    // Get user location and fetch bus stops
+    function getUserLocation() {
+      navigator.geolocation.getCurrentPosition(
+        position => {
+          const location = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          };
+          setUserLocation(location);
+          fetchBusStops(location.lat, location.lng);
+        },
+        error => {
+          console.error("Error getting user location:", error);
+          setUserLocation({ lat: 51.0447, lng: -114.0719 });
+          fetchBusStops("51.0447", "-114.0719");
+          setLoading(false);
+        }
+      );
+    }
+
+    getUserLocation();
+  }, []);
+
+  return busStops.map((busStops, index) =>
     <AdvancedMarker
       key={index}
-      position={{ lat: marker[1], lng: marker[0] }}
+      position={{
+        lat: busStops.location.coordinates[1],
+        lng: busStops.location.coordinates[0]
+      }}
     >
       <img
         src="/images/bustop_blue.webp"
@@ -17,7 +71,7 @@ const StopMarkers = ({ testDataStops }) => {
         height={33}
       />
     </AdvancedMarker>
-  ));
+  );
 };
 
 export default StopMarkers;
