15be6054a8cb8f80a38f4546d541789557745acb
Naiara <naiaralopess@gmail.com>
2024-11-26 16:06:52 -0700

    add pop up for confirmation that email was sent and clear form after

--- Frontend/src/components/ContactUs.js
+++ Frontend/src/components/ContactUs.js
@@ -1,6 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Contact = () => {
+  const [email, setEmail] = useState('');
+  const [subject, setSubject] = useState('');
+  const [message, setMessage] = useState('');
+  const [showPopup, setShowPopup] = useState(false);
+  const formRef = React.useRef(null);
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setShowPopup(true);
+  };
+
+  const closePopup = () => {
+    setShowPopup(false);
+    setEmail('');
+    setSubject('');
+    setMessage('');
+    if (formRef.current) {
+      formRef.current.reset();
+    }
+
+    //e.target.reset();
+  };
+
+
+
   return (
     <div className="max-w-screen-xl mx-auto p-6 sm:flex items-center">
       <div className="sm:w-1/2 p-5">
@@ -11,7 +38,7 @@ const Contact = () => {
       <div className="sm:w-1/2 p-5">
         <div className="text">
           <span className="text-gray-500 border-b-2 border-sky-600 uppercase">Contact Us</span>
-          <form className="space-y-6 my-8">
+          <form className="space-y-6 my-8" ref={formRef} onSubmit={handleSubmit}>
             <div>
               <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">Your Email</label>
               <input
@@ -42,9 +69,27 @@ const Contact = () => {
                 required
               ></textarea>
             </div>
-            <button type="submit" className="w-full sm:w-40 bg-sky-500 text-white px-6 py-3 font-semibold rounded-md hover:bg-red-600 transition duration-200">
+
+            {showPopup && (
+              <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+                <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md relative">
+                  <div className="flex flex-col items-center">
+                    <div className="bg-sky-500 rounded-full p-2 mb-4 absolute -top-6 left-1/2 transform -translate-x-1/2">
+                      <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 text-white" viewBox="0 0 20 20" fill="currentColor">
+                        <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
+                      </svg>
+                    </div>
+                    <h2 className="text-2xl font-bold mb-2">Thank You!</h2>
+                    <p className="text-gray-600 mb-4 text-center">Your Details has been Succesfully Submitted. Thanks!</p>
+                    <button onClick={closePopup} className="w-full sm:w-40 bg-sky-500 text-white px-6 py-2 rounded-md hover:bg-sky-900 transition duration-200">OK</button>
+                  </div>
+                </div>
+              </div>
+            )}
+            <button type="submit" className="w-full sm:w-40 bg-sky-500 text-white px-6 py-3 font-semibold rounded-md hover:bg-sky-900 transition duration-200">
               Send Message
             </button>
+
           </form>
         </div>
       </div>
