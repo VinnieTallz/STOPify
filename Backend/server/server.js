0dd7d8fbeec8ccafd32e9293a7ff02bed7f281d8
VinnieTallz <vin.saini@gmail.com>
2024-11-27 02:39:56 -0700

    server updated port

--- Backend/server/server.js
+++ Backend/server/server.js
@@ -1,36 +1,32 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import showRequests from './showRequests.js'
-import BusStopController from './transitStops/BusStopController.js';
-import RouteLineController from './routeLines/RouteLineController.js';
-import MergedRoutesController from './mergedRoutes/mergedRoutesController.js';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import showRequests from "./showRequests.js";
+import BusStopController from "./transitStops/BusStopController.js";
+import RouteLineController from "./routeLines/RouteLineController.js";
+import MergedRoutesController from "./mergedRoutes/mergedRoutesController.js";
 
+dotenv.config(); // Load environment variables from .env file
 
-dotenv.config();  // Load environment variables from .env file
+const port = process.env.PORT || 10000;
+const app = express();
 
-const port = process.env.PORT || 3001
-const app = express()
-
-app.use(express.json())
+app.use(express.json());
 app.use(cors());
 
-app.use('/api/routeLines', RouteLineController)
-app.use('/api/busStops', BusStopController)
-app.use('/api/mergedRoutes', MergedRoutesController)
+app.use("/api/routeLines", RouteLineController);
+app.use("/api/busStops", BusStopController);
+app.use("/api/mergedRoutes", MergedRoutesController);
 
-
-app.use(express.static('../public_html'))
-app.use(showRequests)
+app.use(express.static("../public_html"));
+app.use(showRequests);
 
 // Start Server
 const server = app.listen(port, () => {
-  console.log('Server listening on port ' + port);
+  console.log("Server listening on port " + port);
 });
 
-server.on('close',() => {
-    console.log('Closing mongo connection')
-    disconnectDb()
-})
-
-
+server.on("close", () => {
+  console.log("Closing mongo connection");
+  disconnectDb();
+});
