feac9a3dd1a9936d28af275d421f87168b1e6f08
VinnieTallz <vin.saini@gmail.com>
2024-11-27 15:30:41 -0700

    query fix

--- Frontend/src/components/Map.js
+++ Frontend/src/components/Map.js
@@ -28,7 +28,7 @@ const MainMap = () => {
       const radius = 5000;
       try {
         const response = await fetch(
-          `http://localhost:10000/api/busStops/nearby?lat=${lat}&lng=${lng}&radius=${radius}`
+          `http://https://stopify-gha7.onrender.com//api/busStops/nearby?lat=${lat}&lng=${lng}&radius=${radius}`
         );
 
         if (response.status === 200) {
