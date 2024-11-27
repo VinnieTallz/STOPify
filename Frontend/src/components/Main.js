214132594c8e171a68103fd614892092dd94ef85
Zahra Karimi <thezahrakarimi1@gmail.com>
2024-11-27 14:18:48 -0700

    Fixing some bug

--- Frontend/src/components/Main.js
+++ Frontend/src/components/Main.js
@@ -5,17 +5,8 @@ import Contact from './ContactUs.js';
 
 const Main = () => {
     return (
-        <div className="container my-4">
-            <div className="row md:mx-5">
-                <div className="col-lg-8">
+        <div className="container">
                     <Map />
-                </div>
-                <div className="col-lg-4">
-                    {/* <TransitInfo /> */}
-                    {/* <About />
-                    <Contact /> */}
-                </div>
-            </div>
         </div>
     );
 };
